a57504a33021d5a67f841005c507cec96939269c
Luis Bonilla <luisbonilla@MacBook-Air-de-Luis.local>
2023-12-02 08:49:03 -0500

    feat: adding Procfile

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,8 @@ export async function main() {
     serve,
     setup(await yamlHandler())
   );
-  server.listen(Number(process.env.PORT_SERVER), () =>
-    console.log(`http://localhost:${Number(process.env.PORT_SERVER)}`)
+  server.listen(Number(process.env.PORT), () =>
+    console.log(`http://localhost:${Number(process.env.PORT)}`)
   );
 }
 
